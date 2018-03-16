5a5629e43903b5c8ed9cd65a78aa7b368a4d5623
Song Wenfeng <xslower@163.com>
2018-03-16 18:40:11 +0800

    3-16

---
+++ quant/gogogo/rl_dqn.py
@@ -0,0 +1,30 @@
+'''dqn '''
+import tensorflow as tf
+from stock_simulator import Simulator
+
+
+def dtype():
+    return tf.float16
+
+
+stock = []
+sim = Simulator(stock)
+
+
+class Dqn(object):
+    def __init__(self):
+        self.decay = 0.9
+        self.input_shape = [300, 6]
+
+    def _dtype(self):
+        return tf.float16
+
+    def _build_net(self):
+        samples = tf.placeholder(dtype(), [None, *self.input_shape], name='samples')
+        q_target = tf.placeholder(dtype(), [None, sim.num_acts], name='q_target')
+
+        cnn1 = tf.keras.layers.Conv1D(filters=8, kernel_size=5, strides=1, input_shape=self.input_shape, padding='valid')(samples)
+        pool1 = tf.keras.layers.MaxPool1D(pool_size=2, strides=1)(cnn1)
+        cnn2 = tf.keras.layers.Conv1D(filters=16, kernel_size=5, strides=1, padding='valid')(pool1)
+        pool2 = tf.keras.layers.MaxPooling1D(pool_size=2, strides=1)(cnn2)
+        
--- quant/gogogo/stock_simulator.py
+++ quant/gogogo/stock_simulator.py
@@ -3,7 +3,7 @@ class Simulator(object):
         self.total_cash = cash
         self.action_space = ['buy', 'sell', 'wait']
         self.num_acts = len(self.action_space)
-        self.stock = 0
+        self.stock_val = 0
         # 配置股票数据
         self.env = env
         self.reset()
@@ -11,13 +11,36 @@ class Simulator(object):
     def reset(self):
         self.cash = self.total_cash
         self.day_idx = 0
-        self.stock = 0
-
-    def reward(self, idx, action):
-        up = self.env[idx]
-
+        self.stock_val = 0
 
     def step(self, idx, action):
-        self.day_idx += 1
+        fee = 0.995
+        min_val = 1000
+        before = self.stock_val + self.cash
+        up = self.env[idx]
+        # self.day_idx += 1
         if action == 0:  # buy
-            if self.cash > 10:
+            if self.cash > min_val:
+                # 买入卖出都扣除0.5％的手续费
+                # 以当天开盘价和收盘价中的最高价买入
+                if up >= 0:
+                    self.stock_val += self.cash * fee
+                else:  # 相当于当天买入就亏损
+                    self.stock_val += self.cash * (fee + up)
+                self.cash = 0
+            else:  # 没钱买就相当于持仓观望
+                self.stock_val += self.stock_val * up
+        elif action == 1:  # sell
+            if self.stock_val > min_val:
+                # 以当天起止两点最低价卖出
+                if up >= 0:
+                    self.cash += self.stock_val * fee
+                else:
+                    self.cash += self.stock_val * (fee + up)
+                self.stock_val = 0
+            else:  # 没股票卖相当于持币观望
+                self.stock_val += self.stock_val * up
+        else:  # wait
+            self.stock_val += self.stock_val * up
+        reward = (self.stock_val + self.cash) / before - 1
+        return self.stock_val, reward
